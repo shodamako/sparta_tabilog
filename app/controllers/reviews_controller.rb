19961e447f302abd5d2039f8ab41d31d33603948
Sho Kodama <madakodama.1go@gmail.com>
2015-07-05 11:14:51 +0900

    reviewのnewメソッドと画面作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,27 @@
 class ReviewsController < ApplicationController
 
   def new
+    @destination = Destination.find(params[:destination_id])
+    @review = Review.new
   end
+
+  def create
+    @review = Review.new(
+      destination_id: params[:destination_id],
+      user_id: vurrent_user.id,
+      body: review_params["body"]
+      )
+    if @review.save
+      redirect_to destination_url(@review.destination)
+    else
+      render :new
+    end
+  end
+
+private
+
+def review_params
+  params.require(:review).permit(:body)
+end
 
 end
